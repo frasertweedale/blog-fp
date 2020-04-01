838dff10b537bd7fa3f1777a6420938b2718e6fe
Fraser Tweedale <frase@frase.id.au>
2020-04-01 22:41:08 +1000

    prepare for launch

---
+++ index.rst
@@ -0,0 +1,10 @@
+*pureblog*
+----------
+
+I am Fraser Tweedale.  I practice and teach functional programming
+(FP).  That's what this blog is all about.
+
+I am `@hackuador`_ on Twitter, and `frasertweedale`_ on GitHub.
+
+.. _@hackuador: https://twitter.com/hackuador
+.. _frasertweedale: https://github.com/frasertweedale
--- site.hs
+++ site.hs
@@ -1,67 +1,156 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+
+import Text.Pandoc.Definition (Pandoc(..), Block(Header, Plain))
+import Hakyll
 
 
---------------------------------------------------------------------------------
+blogTitle, blogDescription, blogAuthorName, blogAuthorEmail, blogRoot :: String
+blogTitle = "pureblog"
+blogDescription = "Applied Functional Programming"
+blogAuthorName = "Fraser Tweedale"
+blogAuthorEmail = "frase@frase.id.au"
+blogRoot = "https://frasertweedale.github.io/blog-fp"
+
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/**" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "*asc" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "index.rst" $ do
+    route $ setExtension "html"
+    compile $ do
+      posts <- loadRecentPosts
+      let homeContext =
+            listField "posts" context (pure posts)
+            `mappend` constField "title" "Home"
+            `mappend` context
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/index.html" homeContext
+        >>= loadAndApplyTemplate "templates/default.html" homeContext
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll ("posts/*" .&&. hasNoVersion)
+      tagCloud <- renderTagCloud 80 120 tags
+      let archiveContext =
+            listField "posts" context (pure posts)
+            `mappend` constField "tagCloud" tagCloud
+            `mappend` constField "title" "Archive"
+            `mappend` context
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveContext
+        >>= loadAndApplyTemplate "templates/default.html" archiveContext
+        >>= relativizeUrls
+
+  -- a version of the posts to use for "recent posts" list
+  match "posts/*" $ version "recent" $ do
+    route $ setExtension "html"
+    compile $
+      getResourceBody >>= saveSnapshot "source"
+
+  tagsRules tags $ \tag pattern -> do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let ctx = constField "tag" tag
+                `mappend` listField "posts" context (pure posts)
+                `mappend` constField "title" (tag <> " posts")
+                `mappend` constField "blogTitle" blogTitle
+                `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ do
+      posts <- loadRecentPosts
+      let postContext =
+            listField "posts" context (pure posts)
+            `mappend` tagsField "tags" tags
+            `mappend` context
+
+      getResourceBody >>= saveSnapshot "source"
+      pandocCompiler
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html" postContext
+        >>= loadAndApplyTemplate "templates/default.html" postContext
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateCompiler
+
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedContext =
+            bodyField "description"
+            `mappend` context
+      posts <- loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
+        >>= fmap (take 10) . recentFirst
+      renderAtom feedConfiguration feedContext posts
 
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateCompiler
+loadRecentPosts :: Compiler [Item String]
+loadRecentPosts =
+  fmap (take 5) . recentFirst =<< loadAll ("posts/*" .&&. hasVersion "recent")
 
 
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+context :: Context String
+context =
+  dateField "date" "%Y-%m-%d"
+  `mappend` headerField "title" "source"
+  `mappend` constField "blogTitle" blogTitle
+  `mappend` defaultContext
+
+
+-- | Parse the item with Pandoc and pull out the first header
+-- Requires a snapshot named @"source"@, taken before any
+-- compilation, e.g.:
+--
+-- @
+-- do
+--   getResourceBody >>= saveSnapshot "source"
+--   pandocCompiler >>= loadAndApplyTemplate ...
+-- @
+--
+headerField
+  :: String           -- ^ Key to use
+  -> Snapshot         -- ^ Snapshot to load
+  -> Context String   -- ^ Resulting context
+headerField key snap = field key $ \item -> do
+  doc <- readPandoc =<< loadSnapshot (itemIdentifier item) snap
+  maybe
+    (fail $ "no header found in " <> show (itemIdentifier item))
+    (fmap (itemBody . writePandoc) . makeItem)
+    (firstHeader (itemBody doc))
+  where
+    firstHeader (Pandoc _ xs) = firstHeader' xs
+    firstHeader' [] = Nothing
+    firstHeader' (Header _ _ ys : _) = Just (Pandoc mempty [Plain ys])
+    firstHeader' (_ : xs) = firstHeader' xs
+
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+  { feedTitle = blogTitle
+  , feedDescription = blogDescription
+  , feedAuthorName = blogAuthorName
+  , feedAuthorEmail = blogAuthorEmail
+  , feedRoot = blogRoot
+  }
